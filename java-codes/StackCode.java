cf23cfa83b9e57dd748f2a2249c2b5150273e800
zzangjae <dondegi5@gmail.com>
2024-07-13 12:13:17 +0900

    add Java StackCode which is example of Java Stack class

---
+++ java-codes/StackCode.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+
+public class StackCode {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        System.out.println(stack.push(1));  // {1}
+        System.out.println(stack.push(2));  // {2, 1}
+
+        System.out.println(stack.empty());  // {2, 1}
+
+        System.out.println(stack.search(1));  // {2, 1}
+        System.out.println(stack.search(3));  // {2, 1}
+
+        System.out.println(stack.pop());  // {1}
+
+        System.out.println(stack.peek());  // {1}
+        System.out.println(stack.pop());  // {}
+
+        System.out.println(stack.empty());  // {}
+
+    }
+
+}
